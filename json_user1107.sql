572d18c8424c8a7ca8688241e44042baa1a4e2a2
minhyeok <>
2022-11-08 23:58:38 +0900

    Java - JsonParsingTest

---
+++ json_user1107.sql
@@ -0,0 +1,10 @@
+CREATE TABLE JSON_USERS(
+    userId number(4),
+    firstName varchar2(128),
+    lastName varchar2(128),
+    emailAddress varchar2(128),
+    phoneNumber varchar2(128),
+    homepage varchar2(128)
+);
+
+select * from json_users;
